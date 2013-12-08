5dffc88b7416ccac9f51b4fef87b90e4462dadca
Debamitro Chakraborti <debamitro@gmail.com>
2013-12-08 12:43:36 +0530

    Adding dataset from srijan, updating crawler with Goose

--- crawler/getTOILinks.py
+++ crawler/getTOILinks.py
@@ -4,18 +4,30 @@
 # pip install html5lib
 
 from bs4 import BeautifulSoup
+from goose import Goose
 import urllib2,re
 
+satabdi_api_key = "7099dd459ad18fb49672e969d398119e3ad519d0"
+debamitro_api_key = "cb958ffa80165e692f5aaa2f72f7537b950eb563"
+ALCHEMY_PREFIX = 'http://access.alchemyapi.com/calls/url/URLGetText?apikey=' + debamitro_api_key + '&outputMode=json&extractLinks=1&url='
+
+def parseUsingAlchemy(dUrl):
+    totalUrl = ALCHEMY_PREFIX + dUrl;
+    #return totalUrl
+    return urllib2.urlopen(totalUrl).read()
+
+def parseUsingGoose(dUrl):
+    return Goose().extract(url=dUrl).cleaned_text
+
 def getLinksFromURL(theURL, linkPattern):
-    req = urllib2.Request(theURL)
-    resp = urllib2.urlopen(req)
-    thePage = resp.read()
-    soup = BeautifulSoup(thePage,'html5lib')
+    soup = BeautifulSoup(urllib2.urlopen(theURL).read(),'html5lib')
     matcher = re.compile(linkPattern)
     for link in soup.find_all('a'):
         linkhref = link.get('href')
         if linkhref != None and linkhref != '' and matcher.match(linkhref):
-            print (linkhref)
+            resData = parseUsingGoose(linkhref)
+            print (resData)
+
 
 import calendar
 
