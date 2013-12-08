883456758508f7abb6853a9221749819db229553
Tarashish Mishra <sunu0000@gmail.com>
2013-12-08 13:07:57 +0530

    Add support for taking date as argument. Output links of all regions.

--- crawler/theHinduScraper.js
+++ crawler/theHinduScraper.js
@@ -1,22 +1,51 @@
 var page = new WebPage();
 
+if (phantom.args.length !== 1) {
+    console.log('Usage: phantomjs theHinduScraper.js <YYYY/MM/DD>');
+    phantom.exit();
+}
+
+var date = phantom.args[0],
+	baseUrl = 'http://www.thehindu.com/archive/web/',
+	url = baseUrl + date,
+	regions = [
+		'Bangalore',
+		'National',
+		'Andhra Pradesh',
+		'Tamil Nadu',
+		'Delhi',
+		'Karnataka',
+		'Thiruvananthapuram',
+		'Kozhikode',
+		'Chennai',
+		'Visakhapatnam',
+		'Vijayawada',
+		'Hyderabad',
+		'Kerala'];
+
 // Open our page.
-page.open('http://www.thehindu.com/archive/web/2013/12/01/', function(status) {
+page.open(url, function(status) {
 	// Wait 5 seconds for contents to load.
 	setTimeout(function() {
-		// Run some Jquery selection queries to get our links
-		var bangalore = page.evaluate(function() {
-			var links = []
-			$("li[data-section='Bangalore']").each(function() {
-				// Push each link into an array
-				links.push(
-					$(this).children("a").attr('href')
-				);
-			});
-			return links
+		regions.map(function(item){
+			// Run some Jquery selection queries to get our links
+			var articleLinks = page.evaluate(function(item) {
+				var links = [];
+				var query = "li[data-section='" + item + "']";
+				$(query).each(function() {
+					// Push each link into an array
+					links.push(
+						$(this).children("a").attr('href')
+					);
+				});
+				return links;
+			}, item);
+			// Print the contents of the array to the console.
+			console.log(item);
+			console.log("=========================================");
+			console.log(articleLinks);
 		});
-		// Print the contents of the array to the console.
-		console.log(bangalore);
+
 		// Exit phantomjs
 		phantom.exit();
 	}, 5000);
